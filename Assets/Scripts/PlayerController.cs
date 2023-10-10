86e183b4c4c5dd7e3bf25a9a455754937e6f0e91
Nathan Peach <144242721+PeachCube@users.noreply.github.com>
2023-10-10 12:24:11 -0300

    First version of new player input system and added placeholder terrain

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.XR;
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float speed=10f;
+    private float movementX;
+    private float movementY;
+    private bool jumping = false;
+    public float jumpStrength=5f;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        if (Input.GetButton("Horizontal")==true)
+        {
+            gameObject.transform.Translate(new Vector3(inputX*speed,0,0) * Time.deltaTime);      
+        }
+        float inputZ = Input.GetAxis("Vertical");
+        if (Input.GetButton("Vertical") == true)
+        {
+            gameObject.transform.Translate(new Vector3(0, 0, inputZ * speed) * Time.deltaTime);
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && jumping == false)
+        {
+            rb.AddForce(Vector3.up * jumpStrength, ForceMode.Impulse);
+            jumping = true;
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        jumping = false;
+    }
+
+
+}
