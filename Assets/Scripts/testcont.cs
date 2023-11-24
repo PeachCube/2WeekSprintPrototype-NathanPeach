da80c1f022ac6fe15459c11f97cf686f6e038f19
Nathan Peach <144242721+PeachCube@users.noreply.github.com>
2023-11-24 09:48:58 -0500

    Ver 3.03; Added some comments and built the game

--- Assets/Scripts/testcont.cs
+++ Assets/Scripts/testcont.cs
@@ -26,26 +26,26 @@ public class testcont : MonoBehaviour
         {
             Ymove.y = 0;
         }
+    //Prevents the value of Ymove.y from dropping into negative values while the player is grounded.
     }
     void Update()
     {
-        grounded = Mover.isGrounded;
+        grounded = Mover.isGrounded;//Mover.isGrounded detects collision with the ground, and returns a boolean value. 
         Xmove = transform.forward * Input.GetAxis("Vertical");
         Zmove = transform.right * Input.GetAxis("Horizontal");
         Ymove.y += Manager.instance.gravity * Time.deltaTime;
         
         Mover.Move(speed * Time.deltaTime * (Xmove + Zmove));
-        //constantly moving the player. When movement inputs are detected, Xmove and Zmove change
-        //their values which causes the player to move since the values inside the Move() function have changed.
-        
+        //Mover.Move is constantly running because it's inside Update(), but will only move the player
+        //when inputs are detected, because the values given to Mover.Move have changed from zero.
         
         if (Input.GetKeyDown(KeyCode.Space) && grounded)
         {
             Ymove.y += Mathf.Sqrt(-jumpStrength * Manager.instance.gravity);
         }
         Mover.Move(Ymove * Time.deltaTime);
-        //constantly applying gravity to the player. When a jump input is detected, the value of Ymove changes,
-        //cancelling out the force of gravity and allowing the player to move upwards.
+        //Constantly applying gravity to the player. When a jump input is detected, and the player collider is touching the ground,
+        //the value of Ymove changes, cancelling out the force of gravity and allowing the player to move upwards.
         if (Input.GetKey(KeyCode.LeftShift))
         {
             speed = runSpeed;
@@ -73,5 +73,7 @@ public class testcont : MonoBehaviour
             gemSFX.pitch = 1.4f;
             gemSFX.Play(0);
         }
+        //Only handles the sound effects for collisions with gems. The source of the sound effect comes from
+        //the player and not from the gems themselves.
     }
 }
